31937dee1ec7790c01a06f6bec7a8f49d1ef200f
Luozhen <luozhen523cherish@outlook.com>
2016-07-04 10:19:28 +0800

    task4 submit
    nural network practice

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -82,16 +82,26 @@ J = sum(sum(y1 .* log(a3) + (1 - y1) .* log(1 - a3))) / m * (-1) + lambda / (2 *
 % J = J / m * (-1) + lambda / (2 * m) * (sum(sum(Theta1tmp)) + sum(sum(Theta2tmp)));
 
 % part 2: solution
-delta_2 =
-delta_3 = a3 - y1;
+delta_2 = zeros(size(Theta1));
+delta_3 = zeros(size(Theta2));
+bigDelta_1 = zeros(size(Theta1));
+bigDelta_2 = zeros(size(Theta2));
+
 for t = 1:m,
-    z_2 = Theta1 * X(t,:)';
+    a_1 = X(t,:)';
+    z_2 = Theta1 * a_1;
     a_2 = sigmoid(z_2);
     a_2 = [1; a_2];
     z_3 = Theta2 * a_2;
     a_3 = sigmoid(z_3);
-    delta_3 = a_3 - y1(:,i);
-    delta_2 = Theta2(:,[2:end])' * delta_3 .* sigmoidGradient(z_2);
+    delta_3 = a_3 - y1(t,:)';
+    delta_2 = (Theta2(:,[2:end])' * delta_3) .* sigmoidGradient(z_2);
+    bigDelta_1 += delta_2 * a_1';
+    bigDelta_2 += delta_3 * a_2';
+theta11 = [zeros(size(Theta1)(1), 1) Theta1(:,[2:end]) * lambda];
+theta22 = [zeros(size(Theta2)(1), 1) Theta2(:,[2:end]) * lambda];
+Theta1_grad = bigDelta_1 / m + theta11;
+Theta2_grad = bigDelta_2 / m + theta22;
 
 % -------------------------------------------------------------
 
